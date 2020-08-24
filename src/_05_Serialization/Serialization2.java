ca1f28ea5bd63bd4595aff32592544ac36910c07
acutiepieg <aryahigup@gmail.com>
2020-08-24 16:56:21 -0700

    Finished Serialization

---
+++ src/_05_Serialization/Serialization2.java
@@ -0,0 +1,14 @@
+package _05_Serialization;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Serialization2 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
